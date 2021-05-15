22025121cb76aaece7ec7a6d8618f144c78433c0
ssardaev <ser26996652009@yandex.ru>
2021-05-15 22:57:32 +0300

    constructor

--- src/main/java/page/ReestrComplexPage.java
+++ src/main/java/page/ReestrComplexPage.java
@@ -1,4 +1,14 @@
 package page;
 
+import factory.Field;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ReestrComplexPage {
+
+    public Field complexField;
+
+    public ReestrComplexPage(WebDriver driver) {
+        this.complexField = new Field(driver, By.xpath("(//*[@class=\"col-lg-5 ListData__list__cell--2VXt_ ListData__list__cell--overflow--w5T7A\"])[1]"));
+    }
 }
