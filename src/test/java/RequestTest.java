adcbb975c0bff7ee544edc1ac7fde817689d1a94
ssardaev <ser26996652009@yandex.ru>
2021-05-16 22:10:49 +0300

    new method (zayavka sozdaetsa)

--- src/test/java/RequestTest.java
+++ src/test/java/RequestTest.java
@@ -3,6 +3,7 @@ import generator.PropertyLoader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -62,8 +63,20 @@ public class RequestTest {
         requestPage.downloadFileOsnovField.sendKeys(text);
         //requestPage.addDocOsnovButton.click();
         requestPage.sformStatmentButton.click();
+    }
 
+    @Test(description = "заявка создается")
+    public void create(){
+        driver.get(new PropertyLoader().getProperty("url.requestTest"));
+        autorizationPage.emailField.sendKeys(new PropertyLoader().getProperty("login"));
+        autorizationPage.passwordField.sendKeys(new PropertyLoader().getProperty("password"));
+        autorizationPage.autorizationButton.click();
 
+        requestPage.newRequestButton.click();
+        requestPage.dduButton.click();
+        requestPage.registrDduButton.click();
+        requestPage.createRequestButton.click();
+        Assert.assertTrue(requestPage.statusNovayaField.isDisplayed());
 
     }
 }
